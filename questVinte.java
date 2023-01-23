1005d77bb365c8779da342d31fc5f4b4c4671a84
nickolas-silva <nickolas.silva@alunos.ufersa.edu.br>
2023-01-23 09:25:50 -0300

    fix

--- questVinte.java
+++ questVinte.java
@@ -3,6 +3,25 @@ import java.util.Scanner;
 public class questVinte{
   private static Scanner scanner = new Scanner(System.in);
   public static void main(String[] args){
-    
+    System.out.println("Qual o investimento inicial: ");
+    double invi = scanner.nextDouble();
+    System.out.println("Valor mensal? ");
+    double mensa = scanner.nextDouble();
+    System.out.println("Quantos meses? ");
+    int meses = scanner.nextInt();
+    System.out.println("Taxa de juros ao mes? ");
+    float juros = scanner.nextFloat();
+
+
+    double saldoa = invi;
+    for(int i =1; i<=meses;i++)
+    {
+      saldoa += mensa;
+      double rm = saldoa * juros;
+      saldoa = saldoa + rm;
+      System.out.printf("O saldo do mes foi de: %.2f \n", saldoa);
+      System.out.printf("O rendimento mensal foi de: %.2f\n", rm);
+      System.out.println();
+    }
   }
 }
